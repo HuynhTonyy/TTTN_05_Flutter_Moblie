661669de6a9c1f8d4d529e721402528aafb191fa
TriTonyyy <quangtri987@gmail.com>
2024-10-18 13:10:13 +0700

    Update homeScreen
    Add navigator,
    Adjust Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tttn_05_flutter_mobile/Screens/predictionScreen.dart';
 import 'Screens/homeScreen.dart';
-import 'Screens/predictionScreen.dart';
+// import 'Screens/predictionScreen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
+      routes: {
+        '/predictionScreen':(context) => PredictionScreen(),
+        '/homeScreen':(context) => HomeScreen(),
+      },
     );
   }
 }
