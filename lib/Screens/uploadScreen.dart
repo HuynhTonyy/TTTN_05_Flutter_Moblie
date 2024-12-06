5a1f8faaadafa52b62a00a66b8cf15fdb0275cbc
HuynhTonyy <107403128+HuynhTonyy@users.noreply.github.com>
2024-12-06 13:31:13 +0700

    handle missing data

--- lib/Screens/uploadScreen.dart
+++ lib/Screens/uploadScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
@@ -169,15 +170,15 @@ class _MyHomePageState extends State<MyHomePage> {
         'school_MS': row[1] == 'MS' ? 1 : 0,
         'sex_F': row[2] == 'F' ? 1 : 0,
         'sex_M': row[2] == 'M' ? 1 : 0,
-        'age': row[3],
+        'age': row[3] != "" ? row[3] : 0,
         'address_R': row[4] == 'M' ? 1 : 0,
         'address_U': row[4] == 'U' ? 1 : 0,
         'famsize_GT3': row[5] == 'GT3' ? 1 : 0,
         'famsize_LE3': row[5] == 'LE3' ? 1 : 0,
         'Pstatus_A': row[6] == 'A' ? 1 : 0,
         'Pstatus_T': row[6] == 'T' ? 1 : 0,
-        'Medu': row[7],
-        'Fedu': row[8],
+        'Medu': row[7] != "" ? row[7] : 0,
+        'Fedu': row[8] != "" ? row[8] : 0,
         'Mjob_at_home': row[9] == 'at_home' ? 1 : 0,
         'Mjob_health': row[9] == 'health' ? 1 : 0,
         'Mjob_other': row[9] == 'other' ? 1 : 0,
@@ -195,9 +196,9 @@ class _MyHomePageState extends State<MyHomePage> {
         'guardian_father': row[12] == 'father' ? 1 : 0,
         'guardian_mother': row[12] == 'mother' ? 1 : 0,
         'guardian_other': row[12] == 'other' ? 1 : 0,
-        'traveltime': row[13],
-        'studytime': row[14],
-        'failures': row[15],
+        'traveltime': row[13] != "" ? row[13] : 0,
+        'studytime': row[14] != "" ? row[14] : 0,
+        'failures': row[15] != "" ? row[15] : 0,
         'schoolsup_yes': row[16] == 'yes' ? 1 : 0,
         'schoolsup_no': row[16] == 'no' ? 1 : 0,
         'famsup_yes': row[17] == 'yes' ? 1 : 0,
@@ -214,15 +215,15 @@ class _MyHomePageState extends State<MyHomePage> {
         'internet_no': row[22] == 'no' ? 1 : 0,
         'romantic_yes': row[23] == 'yes' ? 1 : 0,
         'romantic_no': row[23] == 'no' ? 1 : 0,
-        'famrel': row[24],
-        'freetime': row[25],
-        'goout': row[26],
-        'Dalc': row[27],
-        'Walc': row[28],
-        'health': row[29],
-        'absences': row[30],
-        'G1': row[31],
-        'G2': row[32],
+        'famrel': row[24] != "" ? row[24] : 0,
+        'freetime': row[25] != "" ? row[25] : 0,
+        'goout': row[26] != "" ? row[26] : 0,
+        'Dalc': row[27] != "" ? row[27] : 0,
+        'Walc': row[28] != "" ? row[28] : 0,
+        'health': row[29] != "" ? row[29] : 0,
+        'absences': row[30] != "" ? row[30] : 0,
+        'G1': row[31] != "" ? row[31] : 0,
+        'G2': row[32] != "" ? row[32] : 0,
       };
       formattedCSV.add(formattedRow);
     }
@@ -366,112 +367,116 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     return true;
   }
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black87,size: 30,),
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black87,
+              size: 30,
+            ),
             onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => MenuScreen()),
                 )),
-      title: Text(
-        widget.title,
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        elevation: 10,
       ),
-      backgroundColor: Colors.blueAccent,
-      centerTitle: true,
-       elevation: 10,
-      
-    ),
-    
-    body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.blue.shade200,
-            Colors.blue.shade200
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade200, Colors.blue.shade200],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Scrollable Table Container
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 10,
-                      offset: Offset(0, 5),
-                    ),
-                  ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Scrollable Table Container
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 10,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: ScrollableTable(data: dataOutput),
                 ),
-                child: ScrollableTable(data: dataOutput),
               ),
-            ),
-            SizedBox(height: 20),
-            // Upload and Download Buttons
-            Column(
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    onPressed: () => uploadCSV(),
-                    icon: Icon(Icons.upload_file, color: Colors.white,size: 30,),
-                    label: Text(
-                      "Upload CSV",
-                      style: TextStyle(fontSize: 25, color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 34, 220, 84),
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+              SizedBox(height: 20),
+              // Upload and Download Buttons
+              Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      onPressed: () => uploadCSV(),
+                      icon: Icon(
+                        Icons.upload_file,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      label: Text(
+                        "Upload CSV",
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 34, 220, 84),
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 10),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    onPressed: () => downloadCSV(csvData),
-                    icon: Icon(Icons.download, color: Colors.white, size: 30),
-                    label: Text(
-                      "Download CSV",
-                      style: TextStyle(fontSize: 25, color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      onPressed: () => downloadCSV(csvData),
+                      icon: Icon(Icons.download, color: Colors.white, size: 30),
+                      label: Text(
+                        "Download CSV",
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
 
 class ScrollableTable extends StatelessWidget {
